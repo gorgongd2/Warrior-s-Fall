768c0f68e36a0a6e1d9f33318c0ebcafb10bb2c7
JohnMay99 <47142129+JohnMay99@users.noreply.github.com>
2019-02-19 15:13:26 +0100

    testbackgroundscroll

---
+++ Assets/backgroundScroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScroll : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.up * newPosition;
+    }
+}
+//public int scrollspeed;
+//public float tileSizeZ;
+//public Transform background;
+//private Vector3 position;
+
+//// Start is called before the first frame update
+//void Start()
+//{
+//    position = background.position;
+//}
+
+//// Update is called once per frame
+//void Update()
+//{
+//    float newPosition = Mathf.Repeat(Time.time * scrollspeed, tileSizeZ);
+//    transform.position = position + Vector3.forward * newPosition;
+//}
