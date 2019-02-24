2aa7557386bbdd356dc7c2448dbefbfb73ce0f6c
Ali Muhammad Kashif <hamkuas@gmail.com>
2019-02-24 20:48:44 +0100

    Added Music

---
+++ Assets/ContinueVelocity.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinuousVelocity : MonoBehaviour
+{
+    // The Velocity
+    public Vector2 velocity;
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
