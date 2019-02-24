aa79ec50f2c68e0e84f6282113b86f3b4aff9862
JohnMay99 <47142129+JohnMay99@users.noreply.github.com>
2019-02-24 18:11:54 +0100

    loadingLevels

--- Assets/VengeFul.cs
+++ Assets/VengeFul.cs
@@ -7,6 +7,12 @@ public class VengeFul : MonoBehaviour
     public int damage = 1;
     public float speed = 10;
     public Rigidbody2D rb;
+    private takeDamage takeDmg;
+
+    void Awake()
+    {
+        takeDmg = GetComponent<takeDamage>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,4 +25,24 @@ public class VengeFul : MonoBehaviour
     {
 
     }
-}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Wall")
+        {
+            return;
+        }
+
+        if (col.tag == "Attack")
+        {
+            Destroy(col.gameObject);
+        }
+
+
+        if (takeDmg.health < 1 || col.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+ }
