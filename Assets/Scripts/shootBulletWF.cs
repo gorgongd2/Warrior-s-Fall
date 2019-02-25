adb689416174e718dc582f9146fee7fc9f43f90e
JohnMay99 <47142129+JohnMay99@users.noreply.github.com>
2019-02-25 20:01:16 +0100

    menu

--- Assets/Scripts/shootBulletWF.cs
+++ Assets/Scripts/shootBulletWF.cs
@@ -8,10 +8,11 @@ public class shootBulletWF : MonoBehaviour
     public Transform blastSpawn;
     public float fireRate;
     private float nextFire;
+    public bool canFire = true;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        if (Input.GetButton("Fire1") && Time.time > nextFire && canFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate(bullet, blastSpawn.position, blastSpawn.rotation);
