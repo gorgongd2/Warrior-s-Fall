8d2d6f884aaf76a0370f931859ce4ab086beabce
JohnMay99 <47142129+JohnMay99@users.noreply.github.com>
2019-03-01 17:43:25 +0100

    gameOver

---
+++ Assets/Scripts/GameOverQuit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverQuit : MonoBehaviour
+{
+    public void loadStart(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Player : MonoBehaviour
 {
@@ -41,6 +43,7 @@ public class Player : MonoBehaviour
         if (takeDmg.health < 1)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(6);
         }
     }
 
